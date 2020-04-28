b67a9e3556d007b9b23ef78d1d43a05b019a3e5a
waveshare <36142105+waveshare@users.noreply.github.com>
2020-04-28 16:13:20 +0800

    Update ina219.py

--- ups_display/ina219.py
+++ ups_display/ina219.py
@@ -189,12 +189,18 @@ if __name__=='__main__':
         bus_voltage = ina219.getBusVoltage_V()             # voltage on V- (load side)
         shunt_voltage = ina219.getShuntVoltage_mV() / 1000 # voltage between V+ and V- across the shunt
         current = ina219.getCurrent_mA()                   # current in mA
+        power = ina219.getPower_W()                        # power in W
+        p = (bus_voltage - 6)/2.4*100
+        if(p > 100):p = 100
+        if(p < 0):p = 0
 
         # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
-        print("PSU Voltage:   {:6.3f} V".format(bus_voltage + shunt_voltage))
-        print("Shunt Voltage: {:9.6f} V".format(shunt_voltage))
+        #print("PSU Voltage:   {:6.3f} V".format(bus_voltage + shunt_voltage))
+        #print("Shunt Voltage: {:9.6f} V".format(shunt_voltage))
         print("Load Voltage:  {:6.3f} V".format(bus_voltage))
         print("Current:       {:9.6f} A".format(current/1000))
+        print("Power:         {:6.3f} W".format(power))
+        print("Percent:       {:3.1f}%".format(p))
         print("")
         
         time.sleep(2)
